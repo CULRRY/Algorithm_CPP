730e79a133aabf0ded6a6ffec2283bfa06ef2a8f
CULRRY <39365034+CULRRY@users.noreply.github.com>
2022-10-30 09:08:45 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 5872 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int M, N;
+    cin >> M >> N;
+    vector<int> v(N+1);
+    v[0] = 0, v[1] = 0;
+    for (int i = 2; i <= N; i++)
+    {
+        v[i] = i;
+    }
+
+    for (int i = 2; i <= sqrt(N); i++)
+    {
+        if (v[i] == 0)
+            continue;
+        
+        for (int j = i*2; j <= N; j += i)
+        {
+            v[j] = 0;   
+        }
+    }
+
+    for (int i = M; i <= N; i++)
+    {
+        if (v[i] != 0)
+            cout << v[i] << "\n";
+    }
+
+    return 0;
+}
