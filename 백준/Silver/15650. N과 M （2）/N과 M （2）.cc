709d1849c37affefcf0e39ea1352f53132bbfd8d
CULRRY <39365034+CULRRY@users.noreply.github.com>
2022-11-13 16:07:02 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+void print(int start, int depth, string s)
+{
+    if (depth == M)
+    {
+        cout << s << "\n";
+        return;
+    }
+    else if (start > N)
+        return;
+
+    for (int i = start; i <= N; i++)
+    {
+        print(i + 1, depth + 1, s + to_string(i) + " ");
+    }
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+
+    print(1, 0, "");
+    return 0;
+}
