ab87f4ee5964f3f61aa392e7b0c51cb676207f33
CULRRY <39365034+CULRRY@users.noreply.github.com>
2022-11-15 18:51:59 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int M, N;
+vector<int> v;
+vector<int> arr;
+vector<bool> canUse;
+vector<int> previous;
+
+void print(int d)
+{
+    if (d == M)
+    {   
+        for (int a : arr)
+        {
+            cout << a << " ";
+        }
+
+        cout << "\n";
+
+        return;
+    }
+
+    int tmp = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (canUse[i])
+        {
+            if (i > 0 && tmp == v[i])
+                continue;
+                
+            tmp = v[i];
+            arr[d] = v[i];
+            canUse[i] = false;
+            print(d + 1);
+            canUse[i] = true;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+    v.assign(N, 0);
+    arr.assign(M, 0);
+    canUse.assign(N, 1);
+    previous.assign(M, 0);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    print(0);
+
+    return 0;
+}
