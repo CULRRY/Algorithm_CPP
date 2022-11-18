d3dbf29eb4d427a3fd5169d70b53cdfb9fbce4bf
CULRRY <39365034+CULRRY@users.noreply.github.com>
2022-11-19 01:33:21 +0900

    [Gold V] Title: 내려가기, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -8,9 +8,7 @@ int main()
 
     int N;
     cin >> N;
-    
-    vector<int> maxV(3);
-    vector<int> minV(3);
+
     vector<int> minDP(3);
     vector<int> maxDP(3);
 
@@ -20,26 +18,15 @@ int main()
         cin >> first >> second >> third;
         if (i == 1)
         {
-            minDP = maxDP = minV = maxV = {first, second, third};
+            minDP = maxDP = {first, second, third};
             continue;
         }
 
-        minDP[0] = std::min(minV[0], minV[1]) + first;
-        minDP[1] = *min_element(minV.begin(), minV.end()) + second;
-        minDP[2] = std::min(minV[1], minV[2]) + third;
-
-        minV = vector<int>(minDP);
-
-        maxDP[0] = std::max(maxV[0], maxV[1]) + first;
-        maxDP[1] = *max_element(maxV.begin(), maxV.end()) + second;
-        maxDP[2] = std::max(maxV[1], maxV[2]) + third;
-
-        maxV = vector<int>(maxDP);
+        minDP = {std::min(minDP[0], minDP[1]) + first, *min_element(minDP.begin(), minDP.end()) + second, std::min(minDP[1], minDP[2]) + third};
+        maxDP = {std::max(maxDP[0], maxDP[1]) + first, *max_element(maxDP.begin(), maxDP.end()) + second, std::max(maxDP[1], maxDP[2]) + third};
     }
 
     cout << *max_element(maxDP.begin(), maxDP.end()) << " " << *min_element(minDP.begin(), minDP.end());
-
-
 
     return 0;
 }
