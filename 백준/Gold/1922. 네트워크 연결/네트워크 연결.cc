8bba9fd037014050c9cbb5822f5d1afb01356a18
CULRRY <39365034+CULRRY@users.noreply.github.com>
2022-12-08 08:34:03 +0900

    [Gold IV] Title: 네트워크 연결, Time: 40 ms, Memory: 3080 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -1,74 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-constexpr int INF = 987654321;
-int adj[1001][1001] = {};
-
-int n, m;
-
-int prim(int n, int adj[][1001])
+struct Edge
 {
-    int distance[n+1] = {INF, };
-    int vnear;
-    int cost;
-    for (int i = 2; i <= n; i++)
+    int w;
+    int v1, v2;
+};
+int n, m;
+int disjoint[1001] = {0, };
+
+int Find(int i)
+{
+    while (disjoint[i] != i)
+        i = disjoint[i];
+    
+    return i;
+}
+void Union(int p, int q)
+{  
+    disjoint[Find(p)] = Find(q);
+}
+
+bool Equal(int p, int q)
+{
+    return Find(p) == Find(q);
+}
+
+int Kruskal(int n, int m, Edge* edges)
+{
+    int size = 0;
+    int cost = 0;
+    sort(edges, edges + m, [](const Edge& e1, const Edge& e2){return e1.w < e2.w;});
+    for (int i = 1; i <= n; i++)
     {
-        distance[i] = adj[1][i];
+        disjoint[i] = i;
     }
 
-    int cnt = n-1;
-    while(cnt--)
+    int i = 0;
+    while (size < n  - 1)
     {
-        int min = INF;
-        for (int i = 2; i <= n; i++)
+        Edge e = edges[i];
+        int p = e.v1;
+        int q = e.v2;
+        if (!Equal(p, q))
         {
-            if (distance[i] >= 0 && distance[i] < min)
-            {
-                min = distance[i];
-                vnear = i;
-            }
+            Union(p, q);
+            cost += e.w;
+            size++;
         }
-
-        cost += min;
-        distance[vnear] = -1;
-        for (int i = 2; i <= n; i++)
-        {
-            if (adj[i][vnear] < distance[i])
-            {
-                distance[i] = adj[i][vnear];
-            }
-        }
+        i++;
     }
-
 
     return cost;
 }
 
-
 int main()
 {
-    cin >> n >> m;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            adj[i][j] = INF;
-        }
-    }
+    cin >> n >> m;
+    Edge edges[100001];
+
     for (int i = 0; i < m; i++)
     {
-        int s, e, w;
-        cin >> s >> e >> w;
-        adj[s][e] = w;
-        adj[e][s] = w;
+        int v1, v2, w;
+        cin >> v1 >> v2 >> w;
+        edges[i] = {w, v1, v2}; 
     }
-    
-    for (int i = 1; i <= n; i++) adj[i][i] = 0;
 
-    cout << prim(n, adj);
-
-
+    cout << Kruskal(n, m, edges);
 
     return 0;
 }
