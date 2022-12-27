afd51fe39caa40a659455c84acf0f46f8036cb48
CULRRY <39365034+CULRRY@users.noreply.github.com>
2022-12-27 21:41:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 44 ms, Memory: 42004 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,37 +1,39 @@
 #include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(0);
+
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    FASTIO;
 
-    int N, K;
-    cin >> N >> K;
-    vector<pair<int, int>> items(N + 1);
-    vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
-    for (int i = 1; i <= N; i++)
+    int n, k;
+    cin >> n >> k;
+    vector<int> w(n+1);
+    vector<int> v(n+1);
+    for (int i = 1; i <= n; i++)
     {
-        int w, v;
-        cin >> w >> v;
-        items[i] = make_pair(w, v);
+        cin >> w[i] >> v[i];
     }
 
+    vector<vector<int>> dp(n+1, vector<int>(k+1, 0));
 
-    for (int i = 1; i <= N; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= K; j++)
+        for (int j = 0; j <= k; j++)
         {
-            if (j - items[i].first < 0)
-                dp[i][j] = dp[i-1][j];
+            if (j - w[i] >= 0)
+            {
+                dp[i][j] = std::max(dp[i-1][j], dp[i-1][j - w[i]] + v[i]);
+            }
             else
-                dp[i][j] = std::max(dp[i-1][j], dp[i-1][j - items[i].first] + items[i].second);
+            {
+                dp[i][j] = dp[i-1][j];
+            }
         }
     }
 
-    cout << dp[N][K];
-
-
+    cout << dp[n][k];
 
     return 0;
 }
