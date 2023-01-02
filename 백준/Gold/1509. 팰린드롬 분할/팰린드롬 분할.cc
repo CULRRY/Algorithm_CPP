eff26ea131fc19a120c091e39a1ea2d79990ddd1
CULRRY <39365034+CULRRY@users.noreply.github.com>
2023-01-02 20:20:47 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 28 ms, Memory: 2948 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -3,6 +3,26 @@
 
 using namespace std;
 
+void makePal(vector<vector<bool>>& p, int size, string& s)
+{
+    for (int i = 0; i < size; i++)
+    {
+        p[i][i] = true;
+        if (i + 1 < size && s[i] == s[i + 1]) 
+            p[i][i+1] = true;
+    }
+
+    for (int i = 2; i < size; i++)
+    {
+        for (int j = 0; i + j < size; j++)
+        {
+            if (s[j] == s[i + j] && p[j+1][i+j-1]) 
+                p[j][i+j] = true;
+        }
+    }
+}
+
+
 int main()
 {
     FASTIO;
@@ -10,35 +30,20 @@ int main()
     string s;
     cin >> s;
     vector<int> dp(s.size() + 1, 99998788);
+    vector<vector<bool>> pel(s.size() + 1, vector<bool>(s.size()+1, false));
+    makePal(pel, s.size(), s);
     dp[0] = 1;
 
     for (int i = 1; i < s.size(); i++)
     {
         for (int j = 0; j <= i; j++)
         {
-            int left = j;
-            int right = i;
-            bool isPelindrome = true;
-            while (left <= right)
+            if (pel[j][i])
             {
-                if (s[left] != s[right])
-                {
-                    isPelindrome = false;
-                    break;
-                }
-                left++;
-                right--;
-            }
-            if (isPelindrome)
-            {
-                if (j == 0)
-                {
-                    dp[i] = 1;
-                }
+                if ( j == 0)
+                    dp[i] = std::min(dp[i], 1);
                 else
-                {
-                    dp[i] = std::min(dp[j - 1] + 1, dp[i]);
-                }
+                    dp[i] = std::min(dp[i], dp[j - 1] + 1);   
             }
         }
     }
