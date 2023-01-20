1a885c9004659cf9a2eef2784bea35fc52787815
CULRRY <39365034+CULRRY@users.noreply.github.com>
2023-01-20 12:28:55 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+
+    int round = 1;
+    a = (a + 1) / 2;
+    b = (b + 1) / 2;
+    
+    while (a != b)
+    {
+        a = (a+1) / 2;
+        b = (b+1) / 2;
+        round++;
+    }
+
+    answer = round;
+    return answer;
+}
