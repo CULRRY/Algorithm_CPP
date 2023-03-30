7b4eeef96fef1996b282d25425dfdeb936d5e6db
CULRRY <rkaghwns8@naver.com>
2023-03-30 12:59:30 +0900

    [Unrated] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    FASTIO;
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+            cout << " ";
+
+        for (int j = 0; j < i; j++)
+            cout << "*";
+        
+        cout << endl;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+            cout << " ";
+
+        for (int j = 0; j < n -i; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
