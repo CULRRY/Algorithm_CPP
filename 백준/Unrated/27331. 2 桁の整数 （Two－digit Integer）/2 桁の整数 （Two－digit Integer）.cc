7f9fe61a8480452c4392474a44003497caf48040
CULRRY <rkaghwns8@naver.com>
2023-04-02 02:53:01 +0900

    [Unrated] Title: 2 桁の整数 (Two-digit Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    FASTIO;
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << a * 10 + b;
+
+    return 0;
+}
