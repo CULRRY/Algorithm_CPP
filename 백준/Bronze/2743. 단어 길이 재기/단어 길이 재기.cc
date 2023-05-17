d5afbd80a8b12c9316f0bd0ce240192d4e05c09f
CULRRY <rkaghwns8@naver.com>
2023-05-17 20:04:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    FASTIO;
+
+    string input;
+    cin >> input;
+    cout << input.length();
+
+    return 0;
+}
