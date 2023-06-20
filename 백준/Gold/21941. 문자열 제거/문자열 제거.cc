2a1f8dceab6cd10fc88a164692896f623f2fa7da
CULRRY <rkaghwns8@naver.com>
2023-06-20 13:53:19 +0900

    [Gold IV] Title: 문자열 제거, Time: 12 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Gold/21941. 문자열 제거/문자열 제거.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define endl "\n"
+using namespace std;
+
+struct Delete
+{
+    int score;
+    string s;
+};
+
+vector<int> DP(1001, 0);
+string s;
+vector<Delete> x;
+int dp(int idx)
+{
+    if (DP[idx] != 0) return DP[idx];
+    if (idx >= s.size()) return 0;
+    int ret = 0;
+
+    for (Delete d : x)
+    {
+        if (d.s == s.substr(idx, d.s.size()))
+            ret = std::max(ret, dp(idx + d.s.size()) + d.score);
+    }
+
+    ret = std::max(ret, dp(idx+1) + 1);
+
+    return DP[idx] = ret;
+}
+
+int main()
+{
+    FASTIO;
+
+
+    cin >> s;
+
+    int m;
+    cin >> m;
+
+    x.resize(m);
+
+    for (auto& i : x)
+        cin >> i.s >> i.score;
+    
+
+    cout << dp(0);
+
+    
+
+    return 0;
+}
