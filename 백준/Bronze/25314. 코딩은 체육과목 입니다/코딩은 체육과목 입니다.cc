3e6e74087828fd35c77870192a5b4daf7197ce8e
CULRRY <rkaghwns8@naver.com>
2023-07-17 21:48:04 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define endl "\n"
+using namespace std;
+
+int n, s, m;
+int dp[51][2];
+
+int main()
+{
+    FASTIO;
+
+    int n;
+    cin >> n;
+
+    int div = n / 4;
+
+    while (div--)
+    {
+        cout << "long" << " ";
+    }
+
+    cout << "int" << endl;
+    
+
+
+    return 0;
+}
